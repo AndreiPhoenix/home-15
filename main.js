62af2b1eda0c12a55becca5d939ec21a564f831c
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2025-05-27 20:32:15 +0500

    main.js

--- main.js
+++ main.js
@@ -9,3 +9,34 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Предотвращаем автоматическое отображение подсказки
+  e.preventDefault();
+  // Сохраняем событие для использования позже
+  deferredPrompt = e;
+  // Показываем кнопку установки
+  document.getElementById('installButton').style.display = 'block';
+});
+
+document.getElementById('installButton').addEventListener('click', () => {
+  // Скрываем кнопку
+  document.getElementById('installButton').style.display = 'none';
+  // Показываем подсказку установки
+  deferredPrompt.prompt();
+  // Ждем ответа пользователя
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('Пользователь согласился установить PWA');
+    } else {
+      console.log('Пользователь отказался от установки PWA');
+    }
+    deferredPrompt = null;
+  });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  console.log('Приложение успешно установлено');
+});
